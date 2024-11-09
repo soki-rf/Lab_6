9e38e651263f8d410d149413f4fb10a3827cbcaf
Timofey Kanunnikov <135886248+soki-rf@users.noreply.github.com>
2024-11-09 14:56:02 +0300

    Create number_1.cpp

---
+++ number_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int>::iterator Find(vector<int>::iterator first, vector<int>::iterator last, int el){
+    for(vector<int>::iterator i = first; i != last; i++) {
+        if(*i == el){
+            return i;
+        }
+    }
+    return last;
+}
+
+int main(){
+    int n, el;
+    cout<< "Enter n" << endl;
+    cin >> n;
+    cout << "Enter array elements" << endl;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    cout << "Enter el" << endl;
+    cin >> el;
+
+    vector<int>::iterator it = Find(v.begin(), v.end(), el);
+    cout << "Result: ";
+    cout << *it << endl;
+  return 0;
+}
